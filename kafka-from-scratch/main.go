4e9b4a0ca0465dcf4008e74c94cec401fc053819
alexblacknn <alexblacknn@yandex.ru>
2024-10-21 12:09:29 +0300

    change code in main

--- kafka-from-scratch/main.go
+++ kafka-from-scratch/main.go
@@ -25,17 +25,78 @@ type Order struct {
 
 func main() {
 	var wg sync.WaitGroup
+	orders := []*Order{
+		&Order{
+			Offset:  1,
+			OrderID: "0001",
+			UserID:  "00001",
+			Items: []Item{
+				{ProductID: "535", Quantity: 1, Price: 300},
+				{ProductID: "125", Quantity: 2, Price: 100},
+			},
+			TotalPrice: 500.00,
+		},
+		&Order{
+			Offset:  2,
+			OrderID: "0002",
+			UserID:  "00002",
+			Items: []Item{
+				{ProductID: "035", Quantity: 7, Price: 100},
+				{ProductID: "525", Quantity: 1, Price: 500},
+			},
+			TotalPrice: 1200.00,
+		},
+		&Order{
+			Offset:  3,
+			OrderID: "0003",
+			UserID:  "00003",
+			Items: []Item{
+				{ProductID: "035", Quantity: 10, Price: 100},
+				{ProductID: "525", Quantity: 2, Price: 500},
+			},
+			TotalPrice: 2000.00,
+		},
+	}
 
-	wg.Add(1)
-	// запустим консбюмер в горутине
+	wg.Add(2)
+
+	go produce(orders, &wg)
 	go consume(&wg)
+
 	wg.Wait()
 
 }
 
-func consume(wg *sync.WaitGroup) {
+func produce(orders []*Order, wg *sync.WaitGroup) {
 	defer wg.Done()
 
+	for _, order := range orders {
+		// Открываем файл в режиме добавления
+		file, err := os.OpenFile("orders.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatalln("Ошибка при открытии файла:", err)
+		}
+		defer file.Close()
+
+		// Сериализуем объект заказа в JSON
+		orderJSON, err := json.Marshal(order)
+		if err != nil {
+			log.Fatalln("Ошибка при сериализации в JSON:", err)
+		}
+
+		// Добавляем новую строку для удобства чтения
+		orderJSON = append(orderJSON, '\n')
+
+		// Записываем JSON в файл
+		if _, err := file.Write(orderJSON); err != nil {
+			log.Fatalln("Ошибка при записи в файл:", err)
+		}
+		log.Println("Заказ успешно записан в файл.")
+	}
+}
+
+func consume(wg *sync.WaitGroup) {
+	defer wg.Done()
 	// Открываем файл для чтения
 	file, err := os.OpenFile("orders.json", os.O_CREATE|os.O_RDONLY, 0644)
 	if err != nil {
